94dced50846a07b478dc24d640e3ecb00b11f10e
woink <wardprice@gmail.com>
2021-01-04 20:04:18 -0500

    fix async error on waitForSelector

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 	await page.goto('https://developer.mozilla.org/en-US/');
   await page.type('#main-q', 'splice()');
   await page.keyboard.down('Enter')
-  page.waitForSelector('.result')
+  await page.waitForSelector('.result')
   const result = await page.evaluate(() => {
     const topResult = document.querySelector('.result-title')
     return topResult.href
