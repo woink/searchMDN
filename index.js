917cdc979183d2df7534342c7b5c3077a6fcfbaa
woink <wardprice@gmail.com>
2021-01-04 19:59:24 -0500

    capture first result

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const puppeteer = require('puppeteer');
 	await page.goto('https://developer.mozilla.org/en-US/');
   await page.type('#main-q', 'splice()');
   await page.keyboard.down('Enter')
+  page.waitForSelector('.result')
+  const result = await page.evaluate(() => {
+    const topResult = document.querySelector('.result-title')
+    return topResult.href
+  })
+  console.log("Result link: ", result)
 
+  browser.close()
   
 })();
