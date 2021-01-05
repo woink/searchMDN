9b4d40bfea709e4d626de17cf49298f798b4383f
woink <wardprice@gmail.com>
2021-01-05 13:29:35 -0500

    returns description with colored log

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const puppeteer = require('puppeteer');
+const chalk = require('chalk');
 
-(async () => {
-	const browser = await puppeteer.launch({ headless: false });
-	const page = await browser.newPage();
-	await page.goto('https://developer.mozilla.org/en-US/');
-  await page.type('#main-q', 'splice()');
-  await page.keyboard.down('Enter')
-  await page.waitForSelector('.result')
-  const result = await page.evaluate(() => {
-    const topResult = document.querySelector('.result-title')
-    return topResult.href
-  })
-  console.log("Result link: ", result)
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://developer.mozilla.org/en-US/');
+    await page.type('#main-q', 'splice()');
+    await page.keyboard.down('Enter')
+    await page.waitForSelector('.result')
+    const result = await page.evaluate(() => {
+      const topResult = document.querySelector('.result-title')
+      return topResult.href
+    })
+    await page.goto(result)
+    await page.waitForSelector('p')
+    const desc = await page.evaluate(() => {
+      const itemDesc = document.querySelector('p')
+      return itemDesc.innerText
+    })
 
-  browser.close()
-  
-})();
+    console.log(chalk.magentaBright(desc))
+    console.log(chalk.underline.dim(result))
+
+    browser.close()
+  })();
